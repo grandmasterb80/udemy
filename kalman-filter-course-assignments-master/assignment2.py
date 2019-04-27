5d4b3632a179c0b484c5c4cd71b5d4d9459f3ca9
Daniel Baudisch <daniel.baudisch@web.de>
2019-04-27 15:08:18 +0200

    udemy/kalman-filter-course-assignments-master: added original files
    Solutions were renamed, original files were added back.

--- kalman-filter-course-assignments-master/assignment2.py
+++ kalman-filter-course-assignments-master/assignment2.py
@@ -1,69 +1,51 @@
-import sys
 import numpy as np
 from sim.sim1d import sim_run
 
 # Simulator options.
 options = {}
 options['FIG_SIZE'] = [8,8]
-options['CONSTANT_SPEED'] = False
+options['CONSTANT_SPEED'] = True
 
 class KalmanFilter:
     def __init__(self):
         self.v = 0
         self.prev_time = 0
-        self.error = np.matrix([[0.],
-                                [0.]])
-
-        self.tolerance = np.matrix([[0.5],
-                                    [0.5]])
         # Initial State
         self.x = np.matrix([[0.],
                             [0.]])
 
         # Uncertainity Matrix
-        self.P = np.matrix([[1000.0, 0.],
-                            [0., 1000.0]])
+        self.P = np.matrix([[0., 0.],
+                            [0., 0.]])
 
         # Next State Function
-        self.F = np.matrix([[1., 1.],
-                            [0., 1.]])
+        self.F = np.matrix([[0., 0.],
+                            [0., 0.]])
 
         # Measurement Function
-        self.H = np.matrix([[1., 0.]])
+        self.H = np.matrix([[0., 0.]])
 
         # Measurement Uncertainty
-        self.R = np.matrix([[0.01]])
+        self.R = np.matrix([[0.0]])
 
         # Identity Matrix
         self.I = np.matrix([[1., 0.],
                             [0., 1.]])
-
-        self.predict_x = self.x
-
     def predict(self,t):
         # Calculate dt.
-        dt = t - self.prev_time
-        oldx = self.x
+        dt =
         # Put dt into the state transition matrix.
         self.F[0,1] = dt
-        self.x = self.F * self.x
-        self.P = self.F * self.P * np.transpose(self.F)
-        self.predict_x = self.x
+
+        self.P =  *  * np.transpose(self.F)
         return self.x[0,0]
     def measure_and_update(self,measurements,t):
-        dt = t - self.prev_time
+        dt =
         self.F[0,1] = dt
         Z = np.matrix(measurements)
-        y = np.transpose(Z) - self.H * self.x
-        S = self.H * self.P * np.transpose(self.H) + self.R
-        K = self.P * np.transpose(self.H) * np.linalg.inv(S)
-        self.x = self.x + K * y
-        self.P = (self.I - K * self.H) * self.P
-
-        self.error = np.matrix.clip( np.abs(self.x - self.predict_x) - self.tolerance, 0.0, 10.0 )
-        self.P += self.error * 400.0
-        print ("self.error = ", self.error)
-
+        y =
+        S =
+        K =  *  * np.linalg.inv(S)
 
         self.v = self.x[1,0]
         self.prev_time = t
