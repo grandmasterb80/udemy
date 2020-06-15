ba644c7b25edc56a2c53b04d71eafaa98ddb1fd8
Daniel Baudisch <daniel.baudisch@web.de>
2020-06-15 17:35:03 +0200

    udemy: added "how-to-get-source" for the MPC course

--- get_udemy_repos.sh
+++ get_udemy_repos.sh
@@ -7,3 +7,12 @@ if [ -d "machine_learning_examples" ]; then
 else
   git clone https://github.com/lazyprogrammer/machine_learning_examples.git
 fi
+
+if [ -d "mpc-course-assignments_basis" ]; then
+  cd mpc-course-assignments_basis
+  git pull
+  cd ..
+else
+  git clone https://github.com/WuStangDan/mpc-course-assignments.git
+fi
+
