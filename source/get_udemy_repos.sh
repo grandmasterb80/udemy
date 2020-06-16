17d408be43528fadb9e29d3402cffe532d4e4efe
Daniel Baudisch <daniel.baudisch@web.de>
2020-06-16 10:27:55 +0200

    udemy/source: make original source read-only to avoid accidental overwriting
    These files are out of the repo visibility and changes would get lost.

--- source/get_udemy_repos.sh
+++ source/get_udemy_repos.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
 if [ -d "machine_learning_examples" ]; then
+  chmod a-w machine_learning_examples
   cd machine_learning_examples
   git pull
   cd ..
+  chmod a-w machine_learning_examples
 else
   git clone https://github.com/lazyprogrammer/machine_learning_examples.git
+  chmod a-w machine_learning_examples
 fi
 
 if [ -d "mpc-course-assignments_basis" ]; then
+  chmod a+w mpc-course-assignments_basis
   cd mpc-course-assignments_basis
   git pull
   cd ..
+  chmod a-w mpc-course-assignments_basis
 else
   git clone https://github.com/WuStangDan/mpc-course-assignments.git  mpc-course-assignments_basis
+  chmod a-w mpc-course-assignments_basis
 fi
 
