e4817e2cb6fecff52c90f7ef3f338968cfe931b7
Daniel Baudisch <daniel.baudisch@web.de>
2020-09-27 22:25:13 +0200

    udemy/complete_self_driving[...]: added image for exercise & code until lesson 64
    Classic lane detection using some non-AI algorithms is implemented until
    lesson 64.

---
+++ complete_self_driving_car_course-applied_deep_learning/lanes.py
@@ -0,0 +1,47 @@
+import cv2
+import numpy as np
+#import matplotlib.pyplot as plt
+
+
+def canny( image ):
+    gray = cv2.cvtColor( image, cv2.COLOR_RGB2GRAY )
+    blur = cv2.GaussianBlur( gray, (5, 5), 0 )
+    canny = cv2.Canny( blur, 50, 150 )
+    return canny
+
+
+def region_of_interest( image ):
+    height = image.shape[ 0 ]
+    polygons = np.array( [
+        [ ( 200,height ), ( 1100, height ), ( 550, 250 ) ]
+        ] )
+    mask = np.zeros_like( image )
+    cv2.fillPoly( mask, polygons, 255 )
+    masked_image = cv2.bitwise_and( image, mask )
+    return masked_image
+
+def display_lines( image, lines ):
+    line_image = np.zeros_like( image )
+    if lines is not None:
+        for line in lines:
+            print( line )
+            x1, y1, x2, y2 = line.reshape( 4 )
+            cv2.line( line_image, (x1, y1), (x2, y2), (255, 0, 0), 10 )
+    return line_image
+
+image = cv2.imread( "Image/test_image.jpg" )
+lane_image = np.copy( image )
+canny = canny( lane_image )
+canny_roi = region_of_interest( canny )
+lines = cv2.HoughLinesP( canny_roi, 2, 1*np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=5 )
+image_with_lanes = display_lines( lane_image, lines )
+combo_image = cv2.addWeighted( lane_image, 0.8, image_with_lanes, 1.0, 1.0 )
+cv2.imshow( "Source", image);
+cv2.imshow( "Result", canny);
+cv2.imshow( "Region of Interest", canny_roi );
+cv2.imshow( "Lanes", combo_image );
+cv2.waitKey( 0 )
+
+#plt.imshow(image);
+#plt.imshow(canny);
+#plt.show()
