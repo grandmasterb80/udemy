68e906b55e2610223ac09d468bd35dbb3e67d1cf
grandmasterb80 <daniel.baudisch@web.de>
2020-11-16 22:54:47 +0100

    udemy/complete_self_driving...: added code for 13, partially 14 and some helper script to download material

--- source/get_udemy_repos.sh
+++ source/get_udemy_repos.sh
@@ -11,6 +11,7 @@ else
   chmod a-w machine_learning_examples
 fi
 
+
 if [ -d "mpc-course-assignments_basis" ]; then
   chmod a+w mpc-course-assignments_basis
   cd mpc-course-assignments_basis
@@ -20,5 +21,17 @@ if [ -d "mpc-course-assignments_basis" ]; then
 else
   git clone https://github.com/WuStangDan/mpc-course-assignments.git  mpc-course-assignments_basis
   chmod a-w mpc-course-assignments_basis
+fi
+
+
+if [ -d "self-driving-car-sim" ]; then
+  chmod a+w self-driving-car-sim
+  cd self-driving-car-sim
+  git pull
+  cd ..
+  chmod a-w self-driving-car-sim
+else
+  git clone https://github.com/udacity/self-driving-car-sim   self-driving-car-sim
+  chmod a-w self-driving-car-sim
 fi
 
