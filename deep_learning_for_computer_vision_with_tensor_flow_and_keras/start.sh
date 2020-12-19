bc0c9906fec62e97b31156c4ddc580c2ed093060
grandmasterb80 <daniel.baudisch@web.de>
2020-12-19 20:12:06 +0100

    udemy: added code for first lesson of deep learning for CV

---
+++ deep_learning_for_computer_vision_with_tensor_flow_and_keras/start.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+conda activate deep_learning_for_cv_with_tensor_flow_and_keras
+
+# conda create -n deep_learning_for_cv_with_tensor_flow_and_keras pip python=3.6
+# Collecting package metadata (current_repodata.json): done
+# Solving environment: done
+# 
+# ## Package Plan ##
+# 
+#   environment location: /home/baudisch/anaconda3/envs/deep_learning_for_cv_with_tensor_flow_and_keras
+# 
+#   added / updated specs:
+#     - pip
+#     - python=3.6
+# 
+# 
+# The following packages will be downloaded:
+# 
+#     package                    |            build
+#     ---------------------------|-----------------
+#     ca-certificates-2020.10.14 |                0         121 KB
+#     certifi-2020.11.8          |   py36h06a4308_0         147 KB
+#     openssl-1.1.1h             |       h7b6447c_0         2.5 MB
+#     pip-20.3                   |   py36h06a4308_0         1.8 MB
+#     python-3.6.12              |       hcff3b4d_2        29.7 MB
+#     setuptools-50.3.2          |   py36h06a4308_2         723 KB
+#     sqlite-3.33.0              |       h62c20be_0         1.1 MB
+#     wheel-0.36.0               |     pyhd3eb1b0_0          32 KB
+#     ------------------------------------------------------------
+#                                            Total:        36.2 MB
+# 
+# The following NEW packages will be INSTALLED:
+# 
+#   _libgcc_mutex      pkgs/main/linux-64::_libgcc_mutex-0.1-main
+#   ca-certificates    pkgs/main/linux-64::ca-certificates-2020.10.14-0
+#   certifi            pkgs/main/linux-64::certifi-2020.11.8-py36h06a4308_0
+#   ld_impl_linux-64   pkgs/main/linux-64::ld_impl_linux-64-2.33.1-h53a641e_7
+#   libedit            pkgs/main/linux-64::libedit-3.1.20191231-h14c3975_1
+#   libffi             pkgs/main/linux-64::libffi-3.3-he6710b0_2
+#   libgcc-ng          pkgs/main/linux-64::libgcc-ng-9.1.0-hdf63c60_0
+#   libstdcxx-ng       pkgs/main/linux-64::libstdcxx-ng-9.1.0-hdf63c60_0
+#   ncurses            pkgs/main/linux-64::ncurses-6.2-he6710b0_1
+#   openssl            pkgs/main/linux-64::openssl-1.1.1h-h7b6447c_0
+#   pip                pkgs/main/linux-64::pip-20.3-py36h06a4308_0
+#   python             pkgs/main/linux-64::python-3.6.12-hcff3b4d_2
+#   readline           pkgs/main/linux-64::readline-8.0-h7b6447c_0
+#   setuptools         pkgs/main/linux-64::setuptools-50.3.2-py36h06a4308_2
+#   sqlite             pkgs/main/linux-64::sqlite-3.33.0-h62c20be_0
+#   tk                 pkgs/main/linux-64::tk-8.6.10-hbc83047_0
+#   wheel              pkgs/main/noarch::wheel-0.36.0-pyhd3eb1b0_0
+#   xz                 pkgs/main/linux-64::xz-5.2.5-h7b6447c_0
+#   zlib               pkgs/main/linux-64::zlib-1.2.11-h7b6447c_3
+# 
+# 
+# Proceed ([y]/n)? y
+# 
+# 
+# Downloading and Extracting Packages
+# ca-certificates-2020 | 121 KB    | ######################################################################################################################################################################## | 100% 
+# pip-20.3             | 1.8 MB    | ######################################################################################################################################################################## | 100% 
+# wheel-0.36.0         | 32 KB     | ######################################################################################################################################################################## | 100% 
+# openssl-1.1.1h       | 2.5 MB    | ######################################################################################################################################################################## | 100% 
+# sqlite-3.33.0        | 1.1 MB    | ######################################################################################################################################################################## | 100% 
+# setuptools-50.3.2    | 723 KB    | ######################################################################################################################################################################## | 100% 
+# python-3.6.12        | 29.7 MB   | ######################################################################################################################################################################## | 100% 
+# certifi-2020.11.8    | 147 KB    | ######################################################################################################################################################################## | 100% 
+# Preparing transaction: done
+# Verifying transaction: done
+# Executing transaction: done
+# #
+# # To activate this environment, use
+# #
+# #     $ conda activate deep_learning_for_cv_with_tensor_flow_and_keras
+# #
+# # To deactivate an active environment, use
+# #
+# #     $ conda deactivate
+# 
